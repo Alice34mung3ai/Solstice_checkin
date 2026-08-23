2785c08684c4098cbd38b0f0ed544bad17e022cb
Alice34mung3ai <alicemungai41@gmail.com>
2026-08-23 18:17:00 +0300

    Fix casing in webhook_service import

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,7 +1,7 @@
 from app.services.checkin_service import CheckinService
 from app.services.notification_service import notification_service, sse_endpoint
 from app.services.queue_service import queue_service
-from app.services.Webhook_service import webhook_service
+from app.services.webhook_service import webhook_service
 
 __all__ = [
     "CheckinService",
